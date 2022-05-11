955a99942565e312ed2ecc41006f4b893d6ac7c1
Maryam Verdon <maryamverdon@gmail.com>
2022-05-11 22:42:38 +0200

    affichage des films

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
-import Header from '../../components/Header';
+import React from 'react'
+import Header from '../../components/Header'
+import DefaultPicture from '../../assets/marvel.png'
+import MovieCard from '../../components/MovieCard'
+ 
+const movies = [
+    {
+        title: 'un titre',
+        originalTitle: 'un titre en vo',
+        author: 'David G',
+        date: '1999',
+        description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        picture: DefaultPicture,
+    },
+    {
+        title: 'un titre',
+        originalTitle: 'un titre en vo',
+        author: 'David G',
+        date: '1999',
+        description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        picture: DefaultPicture,
+    },
+    {
+        title: 'un titre',
+        originalTitle: 'un titre en vo',
+        author: 'David G',
+        date: '1999',
+        description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        picture: DefaultPicture,
+    },
+    {
+        title: 'un titre',
+        originalTitle: 'un titre en vo',
+        author: 'David G',
+        date: '1999',
+        description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        picture: DefaultPicture,
+    },
+    {
+        title: 'un titre',
+        originalTitle: 'un titre en vo',
+        author: 'David G',
+        date: '1999',
+        description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        picture: DefaultPicture,
+    },
+]
 
 function Home(){
     return (
         <div>
             <h1>Page d'accueil</h1>
            <Header />
+
+           {movies.map((movie, index) => (
+            <MovieCard
+                key={`${movie.title}-${index}`}
+                title={movie.title}
+                originalTitle={movie.originalTitle}
+                author={movie.author}
+                date={movie.date}
+                description={movie.description}
+                picture={movie.picture}
+            />
+        ))}
             
         </div>
         
