eeaafcaab5661b07941e147f72fdfb8b7419f0fb
Maryam Verdon <maryamverdon@gmail.com>
2022-05-14 18:27:26 +0200

    modification de l'affichage du composant MovieCard

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types'
 
 function MovieCard({ title, originalTitle, author, date, description, picture }) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <img src={picture} alt="marvel" height={80} width={80} />
-            <span>{title}</span>
-            <span>{originalTitle}</span>
-            <span>{author}</span>
-            <span>{date}</span>
-            <span>{description}</span>
+        <div className="card">
+            <h1>{title}</h1>
+            <img src={picture} alt="marvel" />
+            <p><span>Titre original: </span>{originalTitle}</p>
+            <p><span>Auteur : </span>{author}</p>
+            <p><span>Date: </span>{date}</p>
+            <p className='scroller'>{description}</p>
+            <button>Ajouter favoris</button> 
         </div>
     )
 }
