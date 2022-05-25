35c7de8b80658ddd84cc523ebc7812235995f197
Maryam Verdon <maryamverdon@gmail.com>
2022-05-25 17:31:36 +0200

    ajout d'une pied de page

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Footer(){
+   return(
+    <div className="footerDiv">
+        <p> &copy; 2022 &#45; Maryam VERDON</p>
+    </div>
+   ) 
+}
+
+export default Footer
